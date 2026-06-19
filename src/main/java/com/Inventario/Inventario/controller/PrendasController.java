19299fcd9d22ea1fe09a2433e2952528991462f3
nicorojasf <nico.rojasf@duocuc.cl>
2026-06-18 22:44:54 -0400

    vamos lo pibe

--- src/main/java/com/Inventario/Inventario/controller/PrendasController.java
+++ src/main/java/com/Inventario/Inventario/controller/PrendasController.java
@@ -18,6 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,9 +48,15 @@ public class PrendasController {
     @ApiResponses(value = {
     @ApiResponse(responseCode = "200", description = "Prenda encontrada exitosamente",
         content = @Content(mediaType = "application/json", schema = @Schema(implementation = Prendas.class))),
-    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content),
-    @ApiResponse(responseCode = "404", description = "Prenda no encontrada", content = @Content),
-    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+
+    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"ID debe ser un número valido\"}"))),
+
+    @ApiResponse(responseCode = "404", description = "Prenda no encontrada", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Prenda con ID 47 no encontrada\"}"))),
+
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Ocurrió un error interno del servidor\"}")))
     })
     public ResponseEntity<Prendas> obtenerPorId(@Parameter(description = "ID de la prenda a obtener", example = "1")
                                                 @PathVariable("id") Integer id){
@@ -67,16 +74,18 @@ public class PrendasController {
     @ApiResponse(responseCode = "200", description = "Prendas encontradas exitosamente",
         content = @Content(mediaType = "application/json", 
         array = @ArraySchema(schema = @Schema(implementation = Prendas.class)))), 
-    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content),
-    @ApiResponse(responseCode = "404", description = "No se encontraron prendas", content = @Content),
-    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"No se encontraron prendas\"}"))),
+
+    @ApiResponse(responseCode = "404", description = "No se encontraron prendas", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"No se encontraron prendas registradas\"}"))),
+
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Ocurrió un error interno del servidor\"}")))
     })
     public ResponseEntity<List<Prendas>> listarTodo() {
     return ResponseEntity.ok(prendasService.obtenerTodas());
     }
-
-
-
 
 
 
@@ -86,14 +95,23 @@ public class PrendasController {
         description = "Dentro de un JSON colocas los datos de la prenda que se va a crear. No debes incluir el 'id' ya que se genera automáticamente.",
         required = true))
     @ApiResponses(value = {
-    @ApiResponse(responseCode = "200", description = "Prenda guardada exitosamente",
+    @ApiResponse(responseCode = "201", description = "Prenda guardada exitosamente",
         content = @Content(mediaType = "application/json", schema = @Schema(implementation = Prendas.class))),
-    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content),
-    @ApiResponse(responseCode = "404", description = "No se encontró el recurso relacionado", content = @Content),
-    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+
+    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"No se encontro el recurso relacionado\"}"))),
+
+    @ApiResponse(responseCode = "404", description = "No se encontró el recurso relacionado", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"La categoria especificada no existe\"}"))),
+
+    @ApiResponse(responseCode = "409", description = "Conflicto en la solicitud", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Ya existe una prenda con ese nombre\"}"))),
+
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Ocurrió un error interno del servidor\"}")))
     })
     public ResponseEntity<Prendas> guardar(@RequestBody Prendas prendas) {
-    return ResponseEntity.ok(prendasService.guardar(prendas));
+    return ResponseEntity.status(201).body(prendasService.guardar(prendas));
     }
 
 
@@ -106,9 +124,15 @@ public class PrendasController {
     @ApiResponses(value = {
     @ApiResponse(responseCode = "200", description = "Stock descontado exitosamente",
         content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))), 
-    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content),
-    @ApiResponse(responseCode = "404", description = "Prenda no encontrada", content = @Content),
-    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+
+    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Cantidad debe ser mayor a 0\"}"))),
+
+    @ApiResponse(responseCode = "404", description = "Prenda no encontrada", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Prenda con ID 47 no encontrada\"}"))),
+
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Ocurrió un error interno del servidor\"}")))
     })
     public ResponseEntity<String> descontar(
         @Parameter(description = "ID de la prenda a descontar", example = "1")
@@ -130,9 +154,17 @@ public class PrendasController {
     @ApiResponses(value = {
     @ApiResponse(responseCode = "200", description = "Stock aumentado exitosamente",
         content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))), 
-    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content),
-    @ApiResponse(responseCode = "404", description = "Prenda no encontrada", content = @Content),
-    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+    @ApiResponse(responseCode = "400", description = "Solicitud inválida", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Cantidad debe ser mayor a 0\"}"))),
+
+    @ApiResponse(responseCode = "404", description = "Prenda no encontrada", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Prenda con ID 47 no encontrada\"}"))),
+
+    @ApiResponse(responseCode = "409", description = "Conflicto en la solicitud", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"No se puede aumentar el stock de una prenda que no existe\"}"))),
+
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content (mediaType = "application/json", 
+    examples = @ExampleObject(value = "{\"Mensaje\": \"Ocurrió un error interno del servidor\"}")))
     })
     public ResponseEntity<String> aumentar(
         @Parameter(description = "ID de la prenda a aumentar", example = "1")
